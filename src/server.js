125f8d289b271788b7dddf69a4921fd4473f96ec
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2020-04-10 00:35:00 +0000

    pshing to heroku commit

--- src/server.js
+++ src/server.js
@@ -49,7 +49,7 @@ app.use((err, req, res) => {
 });
 
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
